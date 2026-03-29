726dc68d8686ee592512bd2a1ea09eab5f4097d2
Riya Shukla <riyaa.ssskl@gmail.com>
2026-03-29 22:27:43 +0530

    Add files via upload

---
+++ DP1/climb.java
@@ -0,0 +1,53 @@
+package DP1;
+import java.util.*;
+public class climb {
+   //climbing stairs(recursion)
+   public static int countWays(int n){
+        if(n==0 ){
+            return 1;
+        }
+        if(n < 0){
+            return 0;
+        }
+        return countWays(n-1) + countWays(n-2);
+    }
+    
+    //climbing stairs(memorization)
+    public static int countWays1(int n , int ways[]){
+        if(n==0 ){
+            return 1;
+        }
+        if(n < 0){
+            return 0;
+        }
+        if(ways[n]  != -1){
+            return ways[n];
+        }
+        ways[n] = countWays1(n-1, ways) + countWays1(n-2, ways);
+        return ways[n];
+    }
+
+    //climbing stairs(Tabulation DP)
+    public static int countWaysTab(int n ){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        //tabulation loop
+        for(int i=1; i<=n; i++){
+            if(i==1){
+                dp[i] = dp[i-1];
+            } else {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+    }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int ways[] = new int[n+1]; //mem
+        Arrays.fill(ways, -1); // mem
+        //System.out.println(countWays(n));
+        //System.out.println(countWays1(n, ways));
+        System.out.println(countWaysTab(n));
+    }
+}
